ce55aa1e9158f06fb3217b6914c6b06aa3c1a471
田口直樹 <naoki.ta2206@gmail.com>
2023-08-21 23:16:36 +0900

    いずへきち！！！！

---
+++ static/jquery/data/js/noheader_datatable_original.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    var table = $('#my-table').DataTable({
+        // 横スクロールバーを有効にする (scrollXはtrueかfalseで有効無効を切り替えます)
+        scrollX: false,
+        // 縦スクロールバーを有効にする (scrollYは200, "200px"など「最大の高さ」を指定します)
+        scrollY: 300,
+        // 列設定
+        "bAutoWidth": false,
+        paging: false,
+        "language": {
+        "search": "",            // Removes the 'Search' field label
+        "searchPlaceholder": "Search"   // Placeholder for the search box
+    }
+    });
+});
