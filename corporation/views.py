5fa1a03e6546cea396e274d6dd6289998027c804
田口直樹 <naoki.ta2206@gmail.com>
2023-08-22 17:34:44 +0900

    複数検索で業界ごとに検索できるようにした

--- corporation/views.py
+++ corporation/views.py
@@ -1,16 +1,24 @@
 from django.shortcuts import render
 from .models import Corporation
+from django.db.models import Q
 
 
 def search_corporation(request):
     query = request.GET.get('query', '')  # Get the user input from the query parameter
-    if query is '':
-        corporations = None
 
-    elif query.isdigit():
+    if query.isdigit():
         corporations = Corporation.objects.filter(value=int(query))
+
+    elif query:
+        keywords = query.split()  # クエリを空白で分割してキーワードのリストを作成
+        q_objects = Q()  # 空のQオブジェクトを作成
+
+        for keyword in keywords:
+            q_objects |= Q(name__icontains=keyword)  # 各キーワードに対してQオブジェクトを結合
+
+        corporations = Corporation.objects.filter(q_objects)
     else:
-        corporations = Corporation.objects.filter(name__icontains=query)
+        corporations = None
 
     context = {
         'query': query,
@@ -18,3 +26,4 @@ def search_corporation(request):
     }
 
     return render(request, 'corporation_search.html', context)
+
