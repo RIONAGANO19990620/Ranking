f27fb95c1eaef5e026c03c86b3b298c5d4e4cdce
田口直樹 <naoki.ta2206@gmail.com>
2023-08-28 07:34:06 +0900

    大学リストのデータベース登録に関わる作業

--- university/models.py
+++ university/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class University(models.Model):
     name = models.CharField(max_length=100)
-    value = models.IntegerField()
+    rank = models.TextField()
 
     def __str__(self):
         return self.name
