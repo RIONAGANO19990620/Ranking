7c7265e91e290024a8d69ed35aeffce79a3655de
田口直樹 <naoki.ta2206@gmail.com>
2023-10-27 12:54:55 +0900

    クイズ履歴の追加

--- corporation/models.py
+++ corporation/models.py
@@ -9,3 +9,12 @@ class Corporation(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class QuizHistory(models.Model):
+    question = models.CharField(max_length=255)
+    answer = models.CharField(max_length=255)  # クイズの正解
+    user_answer = models.CharField(max_length=255)  # ユーザーの回答
+    is_correct = models.BooleanField()  # クイズの正誤
+
+    created_at = models.DateTimeField(auto_now_add=True)
